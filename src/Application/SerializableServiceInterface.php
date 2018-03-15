c299338f5e683346f55c903c4d6f92c0d98f5c25
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-15 15:10:39 +0100

    CS

--- src/Application/SerializableServiceInterface.php
+++ src/Application/SerializableServiceInterface.php
@@ -1,8 +1,16 @@
 <?php
+/**
+ * kiwi-suite/contract (https://github.com/kiwi-suite/contract)
+ *
+ * @package kiwi-suite/contract
+ * @see https://github.com/kiwi-suite/contract
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
 declare(strict_types=1);
 namespace KiwiSuite\Contract\Application;
 
 interface SerializableServiceInterface extends \Serializable
 {
-
 }
