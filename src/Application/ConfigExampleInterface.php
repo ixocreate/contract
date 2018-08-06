4e753a2262030d6d1e8aab1d1edaf1c59957015a
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-08-06 17:00:30 +0200

    added ConfigExampleInterface

---
+++ src/Application/ConfigExampleInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * kiwi-suite/contract (https://github.com/kiwi-suite/contract)
+ *
+ * @package kiwi-suite/contract
+ * @see https://github.com/kiwi-suite/contract
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+namespace KiwiSuite\Contract\Application;
+
+interface ConfigExampleInterface
+{
+    /**
+     * @return string
+     */
+    public function configName(): string;
+
+    /**
+     * @return string
+     */
+    public function configContent(): string;
+}
