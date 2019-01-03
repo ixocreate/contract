0412e78c812c389260cce8a90f5c90814dbaf3b5
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-01-03 11:44:27 +0100

    altered method name

--- src/Schema/AdditionalSchemaInterface.php
+++ src/Schema/AdditionalSchemaInterface.php
@@ -10,7 +10,13 @@ declare(strict_types=1);
 namespace Ixocreate\Contract\Schema;
 
 use Ixocreate\Contract\ServiceManager\NamedServiceInterface;
+use Ixocreate\Schema\Builder;
 
-interface AdditionalSchemaInterface extends NamedServiceInterface, SchemaReceiverInterface
+interface AdditionalSchemaInterface extends NamedServiceInterface
 {
+    /**
+     * @param Builder $builder
+     * @return SchemaInterface
+     */
+    public function additionalSchema(Builder $builder): SchemaInterface;
 }
